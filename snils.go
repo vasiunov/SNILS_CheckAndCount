b66e524aa5e37866d37372d4737848bfd67d1ef1
Vasiliy Siunov <vasiunov@gmail.com>
2023-01-22 13:59:03 +0300

    add special cases

--- snils.go
+++ snils.go
@@ -8,6 +8,12 @@ import (
 	"unicode"
 )
 
+/*
+	Realising Algorithm for generating the control number of the Insurance number
+	http://www.consultant.ru/document/cons_doc_LAW_142584/1d9155a863a5949b14b95ecbb536aa84856a2a2e/
+	http://portal.fss.ru:8585/fss/lgot/snils
+*/
+
 func main() {
 
 	var Snils = "112-233-445 95" // checking string
@@ -41,20 +47,25 @@ func countSnils(snils string) bool {
 	snilsLeft := snils[:11]
 	snilsLeft = strings.ReplaceAll(snilsLeft, "-", "")
 
-	// fmt.Println("snilsLeft", snilsLeft) // 112233445
-	//									 i    012345678
-	// 							multiplier    987654321
-
 	var sum, controlSum int
 
 	controlSum, _ = strconv.Atoi(string(snils[12:]))
 
+	// fmt.Println("snilsLeft", snilsLeft) // 112233445
+	//									 i    012345678
+	// 							multiplier    987654321
 	for i, value := range snilsLeft {
 
 		sum += int(value-'0') * (9 - i)
 	}
 
-	if sum == controlSum {
+	sumCheck := sum % 101
+
+	switch sumCheck {
+	case 100:
+		sumCheck = 0
+		fallthrough
+	case controlSum:
 		return true
 	}
 
