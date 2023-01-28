f8544d64bd8c0078091ec8a28c9f329cc158594a
Vasiliy Siunov <vasiunov@gmail.com>
2023-01-28 12:18:06 +0300

    minor fix

--- pkg/snils/readme.md
+++ pkg/snils/readme.md
@@ -1,7 +1,7 @@
 SNILS CheckAndCount
 ================================
 
-Realising Algorithm for generating the control number of the Insurance number (SNILS)
+// Realising Algorithm of checking the control number of the Insurance number (SNILS)
 
 According to http://www.consultant.ru/document/cons_doc_LAW_142584/1d9155a863a5949b14b95ecbb536aa84856a2a2e/
 
--- pkg/snils/snils.go
+++ pkg/snils/snils.go
@@ -1,5 +1,5 @@
 /*
-Package snils Realising Algorithm of ckecking the control number of the Insurance number (SNILS).
+Package snils Realising Algorithm of checking the control number of the Insurance number (SNILS).
 According to http://www.consultant.ru/document/cons_doc_LAW_142584/1d9155a863a5949b14b95ecbb536aa84856a2a2e/.
 Manual check SNILS http://portal.fss.ru:8585/fss/lgot/snils.
 Play code https://go.dev/play/p/IMrxtpMOagJ.
